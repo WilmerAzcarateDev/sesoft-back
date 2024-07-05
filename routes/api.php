a92efc526d4856b773b9149cfabba84860e67e71
WilAzDev <jhonazcaratework@gmail.com>
2024-07-05 16:57:45 -0500

    configuramos jwt

--- routes/api.php
+++ routes/api.php
@@ -1,8 +1,16 @@
 <?php
 
+use App\Http\Controllers\AuthController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
-Route::get('/user', function (Request $request) {
-    return $request->user();
-})->middleware('auth:sanctum');
+Route::group([
+    'middleware'=>'auth',
+    'prefix'=>'auth'
+],function(){
+    Route::post('register',[AuthController::class,'register'])->withoutMiddleware('auth');
+    Route::post('login',[AuthController::class,'login'])->withoutMiddleware('auth');
+    Route::post('me',[AuthController::class,'me']);
+    Route::post('logout',[AuthController::class,'logout']);
+    Route::post('refresh',[AuthController::class,'refresh']);
+});
